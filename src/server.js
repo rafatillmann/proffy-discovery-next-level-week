2c28b9bc518e33520f6daf4f97dbc8fe8e47f152
Rafaela <rafaelatillmann.tr@gmail.com>
2020-09-27 21:47:34 -0300

    new changes

--- src/server.js
+++ src/server.js
@@ -7,10 +7,34 @@ const proffys = [
     {
         name: "aaaa",
     },
-    {
-        name: "bbbbb"
-    }
 ]
+
+const subjects = [
+    "Artes",
+    "Biologia",
+    "Ciências",
+    "Educação física",
+    "Física",
+    "Geografia",
+    "História",
+    "Matemática",
+    "Português",
+    "Quí-mica",
+]
+
+const weekdays = [
+    "Domingo",
+    "Segunda-feira",
+    "Terça-feira",
+    "Quarta-feira",
+    "Quinta-feira",
+    "Sexta-feira",
+    "Sábado",
+]
+
+function getSubject(number){
+    return subjects[number -1];
+}
 
 nunjucks.configure('src/pages', {
     express: app,
@@ -20,7 +44,21 @@ nunjucks.configure('src/pages', {
 app.use(express.static("public"));
 
 app.get("/", (req, res) => res.render("index.html"));
-app.get("/study", (req, res) => res.render("study.html", {proffys}));
-app.get("/give-classes", (req, res) => res.render("give-classes"));
+
+app.get("/study", (req, res) => {
+    const filters = req.query;
+    return res.render("study.html", {proffys, filters, subjects, weekdays})});
+
+app.get("/give-classes", (req, res) => {
+    const data = req.query;
+    const isEmpty = Object.keys(data).length;
+    if(isEmpty > 0){
+        data.subject = getSubject(data.subject);
+        proffys.push(data);
+        return res.redirect("/study");
+    }
+    return res.render("give-classes.html", {subjects, weekdays});
+    }
+    );
 
 app.listen(3333);
